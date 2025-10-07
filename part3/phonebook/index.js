9daaec84e5a7886f2a3da233c5829f8ec4cce19b
elidare <elidare.jjj@gmail.com>
2025-10-07 21:39:14 +0300

    Part 3: 3.7-3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const app = express()
+const morgan = require('morgan')
 
-app.use(express.json())
+const app = express()
 
 let persons = [
     { 
@@ -30,6 +30,14 @@ const generateId = () => {
   const id = Math.floor(Math.random() * 100000000)
   return String(id)
 }
+
+app.use(express.json())
+
+morgan.token('body', function (req, res) { 
+    return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
