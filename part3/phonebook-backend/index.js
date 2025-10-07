ba9b9adfb1461954bf30cd0ed5c4054103e3dd60
elidare <elidare.jjj@gmail.com>
2025-10-07 22:44:48 +0300

    Part 3: 3.11

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 
 const app = express()
 
@@ -32,7 +31,6 @@ const generateId = () => {
   return String(id)
 }
 
-app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 
