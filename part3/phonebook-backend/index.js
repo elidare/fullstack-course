21e73d1d215f3071af67a81de7f68562c8114dc0
elidare <elidare.jjj@gmail.com>
2025-10-07 22:34:46 +0300

    Part 3: 3.9-3.11

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -34,6 +34,7 @@ const generateId = () => {
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('body', function (req, res) { 
     return JSON.stringify(req.body)
