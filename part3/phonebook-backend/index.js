b0d71245867ec8fd31cdcb3b76600faac08e1147
elidare <elidare.jjj@gmail.com>
2025-10-08 23:09:26 +0300

    Part 3: 3.15-3.16

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -5,8 +5,8 @@ const Person = require('./models/person')
 
 const app = express()
 
-app.use(express.json())
 app.use(express.static('dist'))
+app.use(express.json())
 
 morgan.token('body', function (req, res) { 
     return JSON.stringify(req.body)
@@ -20,10 +20,16 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
-    })
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+              response.json(person)
+            } else {
+              response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -51,12 +57,31 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  // todo
-  const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
+app.put('/api/persons/:id', (request, response, next) => {
+    const { name, number } = request.body
 
-  response.status(204).end()
+    Person.findById(request.params.id)
+        .then(person => {
+            if (!person) {
+              return response.status(404).end()
+            }
+
+            person.name = name
+            person.number = number
+
+            return person.save().then(updatedPerson => {
+                response.json(updatedPerson)
+            })
+        })
+        .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
@@ -66,7 +91,27 @@ app.get('/info', (request, response) => {
     `)
 })
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'Unknown endpoint' })
+}
+
+// Handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'Malformatted id' })
+  } 
+
+  next(error)
+}
+
+// Handler of requests with result to errors
+app.use(errorHandler)
+
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
