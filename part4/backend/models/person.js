120259bca647c520974c84bdc2d36ce00413a478
elidare <elidare.jjj@gmail.com>
2025-10-13 13:52:57 +0300

    Part 4: move required checks to schema

--- part4/backend/models/person.js
+++ part4/backend/models/person.js
@@ -17,11 +17,13 @@ mongoose.connect(url)
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
-    minLength: 3
+    minLength: 3,
+    required: true
   },
   number: {
     type: String,
     minLength: 8,
+    required: true,
     validate: {
       validator: function(v) {
         return /^\d{2,3}-\d+$/.test(v)
