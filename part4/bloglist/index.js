4e834d621c1422abbe36d50df7eca987b1d5412e
elidare <elidare.jjj@gmail.com>
2025-10-13 18:54:00 +0300

    Part 4: 4.2

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,34 +1,7 @@
+const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const express = require('express')
-const blogsRouter = require('./controllers/blogs')
 
-const app = express()
-
-app.use(express.json())
-app.use('/api/blogs', blogsRouter)
-
-const unknownEndpoint = (_request, response) => {
-  response.status(404).send({ error: 'Unknown endpoint' })
-}
-
-// Handler of requests with unknown endpoint
-app.use(unknownEndpoint)
-
-const errorHandler = (error, _request, response, next) => {
-  logger.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'Malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
-
-  next(error)
-}
-
-// Handler of requests with result to errors
-app.use(errorHandler)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -1,18 +1,4 @@
 const mongoose = require('mongoose')
-const config = require('../utils/config')
-const logger = require('../utils/logger')
-
-mongoose.set('strictQuery', false)
-
-logger.info('connecting to', config.MONGODB_URI)
-
-mongoose.connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info('connected to MongoDB')
-  })
-  .catch(error => {
-    logger.error('error connection to MongoDB:', error.message)
-  })
 
 const blogSchema = mongoose.Schema({
   title: String,
