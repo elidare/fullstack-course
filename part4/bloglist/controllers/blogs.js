3e9b05a3561b4541f41d9491350934b379e7bf0e
elidare <elidare.jjj@gmail.com>
2025-10-17 21:34:30 +0300

    Part 4: add get :id, put, delete

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -6,11 +6,53 @@ blogsRouter.get('/', async (_request, response) => {
   response.json(blogs)
 })
 
+blogsRouter.get('/:id', async (request, response, next) => {
+  try {
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+      response.json(blog)
+    } else {
+      response.status(404).end()
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
 blogsRouter.post('/', async (request, response, next) => {
   try {
     const blog = new Blog(request.body)
     const result = await blog.save()
     response.status(201).json(result)
+  } catch (error) {
+    next(error)
+  }
+})
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+  } catch (error) {
+    next(error)
+  }
+})
+
+blogsRouter.put('/:id', async (request, response, next) => {
+  try {
+    const newBlog = request.body
+    const blog = await Blog.findById(request.params.id)
+    if (!blog) {
+      return response.status(404).end()
+    }
+
+    blog.title = newBlog.title
+    blog.url = newBlog.url
+    blog.author = newBlog.author
+    blog.likes = newBlog.likes
+
+    const updatedBlog = await blog.save()
+    response.json(updatedBlog)
   } catch (error) {
     next(error)
   }
