e02352f646ca575d6517e47e2876e4f3bfab0c9b
elidare <elidare.jjj@gmail.com>
2025-10-17 22:44:04 +0300

    Part 4: and once new code is saved in commits, delete it

--- part4/backend/README.md
+++
@@ -1,4 +0,0 @@
-The Phonebook exercise from the Fullstack course
-https://fullstackopen.com/en/part3/deploying_app_to_internet
-
-Can be found here: https://fullstack-phonebook-g3q4.onrender.com/
--- part4/frontend/src/components/Input.jsx
+++
@@ -1,9 +0,0 @@
-const Input = (props) => {
-    return (
-      <div>
-          {props.name} <input value={props.value} onChange={props.onChange} />
-      </div>
-    )
-}
-
-export default Input
