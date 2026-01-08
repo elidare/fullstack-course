a8e10ed18b25a641009396625d8bc94892769f39
elidare <elidare.jjj@gmail.com>
2026-01-08 17:18:44 +0200

    Part 8: 8.4

--- part8/graph-project/index.js
+++ part8/graph-project/index.js
@@ -111,7 +111,7 @@ const typeDefs = /* GraphQL */ `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -120,7 +120,8 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) =>
+      args.author ? books.filter((b) => b.author === args.author) : books,
     allAuthors: () => authors,
   },
   Author: {
