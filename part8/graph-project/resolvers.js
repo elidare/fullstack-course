0fc2ebd2003959592616aaa34d39c399f96f388b
elidare <elidare.jjj@gmail.com>
2026-01-11 21:36:09 +0200

    Part 8: 8.13

---
+++ part8/graph-project/resolvers.js
@@ -0,0 +1,104 @@
+const { GraphQLError } = require("graphql");
+const Author = require("./models/author");
+const Book = require("./models/book");
+
+const resolvers = {
+  Query: {
+    bookCount: async () => Book.collection.countDocuments(),
+    authorCount: async () => Author.collection.countDocuments(),
+    allBooks: async (root, args) => {
+      let filtered = Book.find({});
+
+      if (args.author) {
+        // todo
+        filtered = filtered.filter((b) => b.author === args.author);
+      }
+
+      if (args.genre) {
+        // todo
+        filtered = filtered.filter((b) => b.genres.includes(args.genre));
+      }
+
+      return filtered;
+    },
+    allAuthors: async () => Author.find({}),
+  },
+  Author: {
+    bookCount: async (root) => {
+      return Book.find({ author: root.name }).length;
+    },
+  },
+  Mutation: {
+    addBook: async (root, args) => {
+      const titleExists = await Book.exists({ title: args.title });
+
+      if (titleExists) {
+        throw new GraphQLError(`Title must be unique: ${args.title}`, {
+          extensions: {
+            code: "BAD_USER_INPUT",
+            invalidArgs: args.title,
+          },
+        });
+      }
+
+      let author = await Author.findOne({ name: args.author });
+
+      if (!author) {
+        author = new Author({ name: args.author });
+
+        try {
+          await author.save();
+        } catch (error) {
+          throw new GraphQLError(`Saving new author failed: ${error.message}`, {
+            extensions: {
+              code: "BAD_USER_INPUT",
+              invalidArgs: args.author,
+              error,
+            },
+          });
+        }
+      }
+
+      const book = new Book({ ...args, author: author._id });
+
+      try {
+        await book.save();
+      } catch (error) {
+        throw new GraphQLError(`Saving book failed: ${error.message}`, {
+          extensions: {
+            code: "BAD_USER_INPUT",
+            invalidArgs: args.title,
+            error,
+          },
+        });
+      }
+
+      return book.populate("author");
+    },
+    editAuthor: async (root, args) => {
+      const author = await Author.findOne({ name: args.name });
+
+      if (!author) {
+        return null;
+      }
+
+      author.born = args.setBornTo;
+
+      try {
+        await author.save();
+      } catch (error) {
+        throw new GraphQLError(`Saving author failed: ${error.message}`, {
+          extensions: {
+            code: "BAD_USER_INPUT",
+            invalidArgs: args.name,
+            error,
+          },
+        });
+      }
+
+      return author;
+    },
+  },
+};
+
+module.exports = resolvers;
