d0ad2b7b1205ce94c47fc659874af990bc0e009a
elidare <elidare.jjj@gmail.com>
2026-01-28 18:55:05 +0200

    Part 9: 9.3

--- part9/setup-project/bmiCalculator.ts
+++ part9/setup-project/bmiCalculator.ts
@@ -1,8 +1,40 @@
+interface BodyValues {
+  height: number;
+  weight: number;
+}
+
+const parseArguments = (args: string[]): BodyValues => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+  if (args.length > 4) throw new Error("Too many arguments");
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3]),
+    };
+  } else {
+    throw new Error("Provided values are not numbers");
+  }
+};
+
 const calculateBmi = (height: number, weight: number): string => {
+  if (weight <= 0 || height <= 0) {
+    throw new Error("Provided values are not real");
+  }
+
   const ind = weight / (height / 100) ** 2;
   if (ind < 18.5) return "Underweight";
   if (ind < 25) return "Normal range";
   return "Overweight";
 };
 
-console.log(calculateBmi(180, 74));
+try {
+  const { height, weight } = parseArguments(process.argv);
+  console.log(calculateBmi(height, weight));
+} catch (error: unknown) {
+  let errorMessage = "Something bad happened.";
+  if (error instanceof Error) {
+    errorMessage += " Error: " + error.message;
+  }
+  console.log(errorMessage);
+}
--- part9/setup-project/exerciseCalculator.ts
+++ part9/setup-project/exerciseCalculator.ts
@@ -12,6 +12,36 @@ interface Ratings {
   [key: number]: string;
 }
 
+interface Values {
+  target: number;
+  days: number[];
+}
+
+const parseArgumentsCalculator = (args: string[]): Values => {
+  // Assuming nobody will input a long array, let's no have a limit here at the moment
+  if (args.length < 4) throw new Error("Not enough arguments");
+
+  let days: number[] = [];
+  let target: number;
+
+  // Let's go through args one by one and check every. Push to days if a number
+  args.slice(2).forEach((a, ind) => {
+    if (isNaN(Number(a))) {
+      throw new Error("Provided values are not numbers");
+    }
+    if (ind === 0) {
+      target = Number(a);
+    } else {
+      days.push(Number(a));
+    }
+  });
+
+  return {
+    target,
+    days,
+  };
+};
+
 const calculateExercises = (days: number[], target: number): Result => {
   // Assuming target always >=0
   const ratings: Ratings = {
@@ -50,4 +80,13 @@ const calculateExercises = (days: number[], target: number): Result => {
   };
 };
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+try {
+  const { target, days } = parseArgumentsCalculator(process.argv);
+  console.log(calculateExercises(days, target));
+} catch (error: unknown) {
+  let errorMessage = "Something bad happened.";
+  if (error instanceof Error) {
+    errorMessage += " Error: " + error.message;
+  }
+  console.log(errorMessage);
+}
