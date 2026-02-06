e5fa26b9ef3c65eb28b8c6add838b4fb6a55a224
elidare <elidare.jjj@gmail.com>
2026-02-06 22:08:18 +0200

    Part 9: 9.18-20

--- part9/flight-diary-frontend/src/App.tsx
+++ part9/flight-diary-frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
-import type { DiaryEntry } from "./../types";
+import type { DiaryEntry, NewDiaryEntry } from "./../types";
 import diaryService from "./services/diaries";
 import DiariesList from "./components/DiariesList";
+import DiaryForm from "./components/DiaryForm";
 
 const App = () => {
   const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
+  const [notification, setNotification] = useState("");
 
   useEffect(() => {
     const fetchDiaries = async () => {
@@ -15,9 +17,27 @@ const App = () => {
     fetchDiaries();
   }, []);
 
+  const addDiary = async (newDiary: NewDiaryEntry) => {
+    try {
+      const savedEntry = await diaryService.addDiary(newDiary);
+      setDiaries(diaries.concat(savedEntry));
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        setNotification(error.message);
+        setTimeout(() => {
+          setNotification("");
+        }, 5000);
+      }
+    }
+  };
+
   return (
     <>
       <h1>Flight diary</h1>
+      <DiaryForm
+        handleSubmit={addDiary}
+        notification={notification}
+      ></DiaryForm>
       <DiariesList diaries={diaries}></DiariesList>
     </>
   );
--- part9/flight-diary-frontend/src/services/diaries.ts
+++ part9/flight-diary-frontend/src/services/diaries.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import type { DiaryEntry } from "../../types";
+import type { DiaryEntry, NewDiaryEntry } from "../../types";
 
 const baseUrl = "/api/diaries";
 
@@ -8,4 +8,20 @@ const getAllDiaries = async () => {
   return response.data;
 };
 
-export default { getAllDiaries };
+const addDiary = async (newDiary: NewDiaryEntry) => {
+  try {
+    const response = await axios.post<DiaryEntry>(baseUrl, newDiary);
+    return response.data;
+  } catch (error) {
+    // Narrowing Axious error
+    if (axios.isAxiosError(error)) {
+      console.error(error.response);
+      throw new Error(error.response?.data);
+    } else {
+      console.error(error);
+      throw new Error("Something went wrong");
+    }
+  }
+};
+
+export default { getAllDiaries, addDiary };
