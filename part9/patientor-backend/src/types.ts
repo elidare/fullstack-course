f8fd14fb19f1fa5a241c7d4fc98af2ee9f7c3931
elidare <elidare.jjj@gmail.com>
2026-02-13 18:08:19 +0200

    Part 9: 9.23

--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -1,8 +1,6 @@
 import { z } from "zod";
 import { newEntrySchema } from "./utils";
 
-export interface Entry {}
-
 export interface DiagnosisEntry {
   code: string;
   name: string;
@@ -15,7 +13,56 @@ export enum Gender {
   Other = "other",
 }
 
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<DiagnosisEntry["code"]>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+// // Define special omit for unions
+// type UnionOmit<T, K extends string | number | symbol> = T extends unknown
+//   ? Omit<T, K>
+//   : never;
+// // Define Entry without the 'id' property
+// type EntryWithoutId = UnionOmit<Entry, "id">;
+
 export type NewPatientEntry = z.infer<typeof newEntrySchema>;
+
+export type Entry =
+  | HealthCheckEntry
+  | HospitalEntry
+  | OccupationalHealthcareEntry;
 
 export interface PatientEntry extends NewPatientEntry {
   id: string;
